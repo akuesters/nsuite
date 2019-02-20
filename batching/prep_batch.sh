d603be61f1c9a61ae25a6b558a317bc83a2317ee
Alex Peyser <a.peyser@fz-juelich.de>
2019-02-20 10:57:32 +0100

    WIPÃ: files look right

--- batching/prep_batch.sh
+++ batching/prep_batch.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+#set -x -v
+
 ########################### Params
-cells_per_node_sets=(1000 10000)
+cells_per_rank_sets=(1000 10000)
 nodes_min=1
 nodes_max=1024
-node_scaling=2
+nodes_scaling=2
 config=small
 model=ring
 dryrun=true
 tag=default
 ranks_per_node=2
-execpath=/p/project/cslns/exascale/nsuite/benchmarks/engines/busyring/arbor
-partition=osws_tue_pm_large
-cpus_per_task=48
+execpath=$(readlink -f "$(pwd)/../benchmarks/engines/busyring/arbor")
+partition=osws_wed_am_large
+cpus_per_task=24
 ###########################
 
 append() {
@@ -35,38 +37,50 @@ build-sets() {
 }
 
 eval-cmdline() {
-    while $#; do
+    while (($# > 0)); do
         eval $1
         shift
     done
-    output_path="$(pwd)/batch-benchmarks/$tag/$model/$config/$dryrun"
+    output_path=$(readlink -f "$(pwd)")/batch-benchmarks/$tag/$model/$config/$dryrun
     mkdir -p $output_path
 }
 
 do-sed() {
+    echo "Building $2 from $1"
     sed -r \
-        -e "s/@CELLS@/$cells/og" \
-        -e "s/@NAME@/$name/og" \
-        -e "s/@NODES@/$nodes/og" \
-        -e "s/@DRYRUN@/$dryrun/og" \
-        -e "s/@RUNPATH@/$runpath/og" \
-        -e "s/@INPUT@/$input/og" \
-        -e "s/@EXECPATH@/$execpath/og" \
-        -e "s/@PARTITION@/$partition/og" \
-        -e "s/@CPUSPERTASK@/$cpus_per_task/og" \
+        -e "s+@REALRANKS@+$real_ranks+g" \
+        -e "s+@REALNODES@+$real_nodes+g" \
+        -e "s+@CELLS@+$cells+g" \
+        -e "s+@NAME@+$name+g" \
+        -e "s+@NODES@+$nodes+g" \
+        -e "s+@DRYRUN@+$dryrun+g" \
+        -e "s+@RUNPATH@+$runpath+g" \
+        -e "s+@INPUT@+$input+g" \
+        -e "s+@EXECPATH@+$execpath+g" \
+        -e "s+@PARTITION@+$partition+g" \
+        -e "s+@CPUSPERTASK@+$cpus_per_task+g" \
+        -e "s+@RANKS@+$ranks+g" \
         <"$1" >"$2"
 }
 
 build-job() {
-    local cells_per_node=$1; shift
+    local cells_per_ranks=$1; shift
     local nodes=$1; shift
 
-    local cells=$(( cells_per_node * nodes ))
     local ranks=$(( ranks_per_node * nodes ))
+    local cells=$(( cells_per_ranks * ranks ))
+
+    if [[ $dryrun == true ]]; then
+        local real_ranks=1
+        local real_nodes=1
+    else
+        local real_ranks=$ranks
+        local real_nodes=$nodes
+    fi
 
     local input="run-$model-$config"
-    local runpath="$output_path/ranks-$ranks"
-    mkdir -p "$output"
+    local runpath="$cell_output_path/ranks-$ranks"
+    mkdir -p "$runpath"
 
     local name="run-$tag-$model-$config-$dryrun"
 
@@ -80,26 +94,28 @@ build-job() {
 run-job() {
     local nodes=$1; shift
     local ranks=$(( ranks_per_node * nodes ))
-    local runpath="$output_path/ranks-$ranks"
+    local runpath="$cell_output_path/ranks-$ranks"
 
     sbatch "$runpath"/$input.sh
 }
 
 over-nodes() {
-    local cells_per_node=$1; shift
+    local cells_per_ranks=$1; shift
+    local cell_output_path="$output_path/cells-$cells_per_rank"
+    mkdir -p "$cell_output_path"
 
     local nodes
-    for nodes in ${node_sets}
+    for nodes in ${nodes_sets[@]}
     do
-        build-job $cells_per_node $nodes
+        build-job $cells_per_rank $nodes
     done
 }
 
 over-cells() {
-    local cells_per_node
-    for cells_per_node in ${cells_per_node_sets[@]}
+    local cells_per_rank
+    for cells_per_rank in ${cells_per_rank_sets[@]}
     do
-        over-nodes $cells_per_node
+        over-nodes $cells_per_rank
     done
 }
 
