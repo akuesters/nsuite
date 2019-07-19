a23721eb149a271a13d1aea7715d304a62b54684
akuesters <a.kuesters@fz-juelich.de>
2019-07-19 10:15:47 +0200

    update juwels mc and add juwels gpu

--- systems/juwels-gpu.sh
+++ systems/juwels-gpu.sh
@@ -1,23 +1,31 @@
 ### environment ###
 
+# record system name
+ns_sysname="juwels-gpu"
+
 # set up environment for building on the multicore part of juwels
+module purge
 module use /gpfs/software/juwels/otherstages/
-module load Stages/Devel-2018b
+module load Stages/2019a
 
-module load GCC/7.3.0 
-module load CUDA/9.2.88 
-export LD_LIBRARY_PATH="$EBROOTCUDA/lib64/stubs:$LD_LIBRARY_PATH"
-module load MVAPICH2/2.3-GDR
-module load netCDF/4.6.1
-module load CMake/3.12.3
+module load GCC
+module load MVAPICH2
+module load CMake
 
-module load Python/3.6.6
+module load CUDA 
+export MV2_ENABLE_AFFINITY=0
+export MV2_USE_GPUDIRECT_GDRCOPY=0
+
+module load Python/3.6.8
+module load SciPy-Stack/2019a-Python-3.6.8
 ns_python=$(which python3)
 
-# for (core)neuron 
-module load mpi4py/3.0.0-Python-3.6.6
+# for (core)neuron
+module load mpi4py/3.0.1-Python-3.6.8
 module load flex/2.6.4
-module load Bison/.3.1
+
+# for validation tests
+module load netCDF/4.6.3-serial
 
 ### compilation options ###
 
@@ -27,6 +35,9 @@ ns_with_mpi=ON
 
 ns_arb_with_gpu=ON
 ns_arb_arch=skylake-avx512
+ns_arb_modcc=ON
+
+ns_arb_branch=master
 
 ns_makej=20
 
@@ -35,10 +46,12 @@ ns_makej=20
 ns_threads_per_core=2
 ns_cores_per_socket=20
 ns_sockets=1
-ns_threads_per_socket=40
+ns_threads_per_socket=20
 
 # activate budget via jutil env activate -p <cproject> -A <budget> before running the benchmark
 run_with_mpi() {
-    echo ARB_NUM_THREADS=$ns_threads_per_socket srun -n $ns_sockets -N $ns_sockets -c $ns_threads_per_socket $*
-    ARB_NUM_THREADS=$ns_threads_per_socket srun -n $ns_sockets -N $ns_sockets -c $ns_threads_per_socket $*
+    export ARB_NUM_THREADS=$ns_threads_per_socket
+    export OMP_NUM_THREADS=$ns_threads_per_socket
+    echo srun -n$ns_sockets -N1 -c$ns_threads_per_socket "${@}"
+    srun -n$ns_sockets -N1 -c$ns_threads_per_socket "${@}"
 }
